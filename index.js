51e8c39780ee75e28e77e1c913bd6de1b0929176
sungkimcastro <s.kim.castro@gmail.com>
2021-05-19 21:55:02 +0300

    Getting payload from client

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const server = http.createServer((req, res) => {
-  res.end('hello you');
+  const urlParsed = url.parse(req.url, true);
+
+  const pathTrimmed = urlParsed.pathname.replace(/^\/+|\/+$/g, '');
+  const queries = urlParsed.query;
+  const httpMethod = req.method.toLowerCase();
+  const headers = req.headers;
+
+  const decoder = new StringDecoder();
+  let buffer = '';
+
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    res.end('hello you');
+  });
 });
 
 server.listen(3000);
